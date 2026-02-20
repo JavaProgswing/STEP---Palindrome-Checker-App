e6d841d5c28bde3fd4b95d6312e4dcdd26404473
TJP <69581225+JavaProgswing@users.noreply.github.com>
2026-02-20 09:55:56 +0530

    feat: implement Use case 5 - palindrome checks.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,37 +1,37 @@
 /**
  * ============================================================
- * MAIN CLASS - UseCase4PalindromeCheckerApp
+ * MAIN CLASS - UseCase5PalindromeCheckerApp
  * ============================================================
  * <p>
- * Use Case 4: Character Array Based Validation
+ * Use Case 5: Stack Based Palindrome Checker
  * <p>
  * Description:
- * Validates a palindrome by converting the string into
- * a character array and comparing characters using
- * the two-pointer technique.
+ * Validates a palindrome using a Stack data structure
+ * which follows the LIFO principle.
  *
  * @author Developer
- * @version 4.0
+ * @version 5.0
  */
+
+import java.util.Stack;
 
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "madam";
+        String input = "noon";
         System.out.println("Input text: " + input);
 
-        char[] chars = input.toCharArray();
-        int left = 0;
-        int right = chars.length - 1;
-        boolean isPalindrome = true;
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
 
-        while (left < right) {
-            if (chars[left] != chars[right]) {
+        boolean isPalindrome = true;
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            left++;
-            right--;
         }
 
         System.out.print("It is a Palindrome? : ");
